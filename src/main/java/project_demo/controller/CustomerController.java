49a06c79238e9dadd3f9ac870f533a511dfdff20
Alin Moise <alin.moise@fortech.ro>
2020-01-27 09:35:11 +0200

    corect import

--- src/main/java/project_demo/controller/CustomerController.java
+++ src/main/java/project_demo/controller/CustomerController.java
@@ -1,10 +1,7 @@
 package project_demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import project_demo.model.Customer;
 import project_demo.service.CustomerService;
 
